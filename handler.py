67639f192e155da2169a213ea1c7c507a312f1c8
bohdan.t <bohdan.t@halo-lab.team>
2026-01-08 19:27:53 +0200

    fixed

--- handler.py
+++ handler.py
@@ -5,6 +5,7 @@ import runpod
 from inference import get_inference_service
 import cv2
 import base64
+import numpy as np
 
 service = get_inference_service()
 
@@ -13,7 +14,11 @@ def generator_handler(job):
     job_input = job["input"]
     audio_data = job_input.get("audio_data")
     model_name = job_input.get("model_name", "Iris")
-    for frame, _ in service[model_name].generate_frames(audio_data):
+
+    audio_bytes = base64.b64decode(audio_data)
+    aud = np.frombuffer(audio_bytes, dtype=np.int16)
+    aud = aud.astype(np.float32) / 32768.0
+    for frame, _ in service[model_name].generate_frames(aud):
         _, buffer = cv2.imencode(".jpg", frame)
         jpg_as_text = base64.b64encode(buffer).decode("utf-8")
         yield {"frame": jpg_as_text}
